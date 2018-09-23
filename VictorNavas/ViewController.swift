e5cf4cdeaa88c6b8890dfcf70b88cd294aa70c7a
Victor Navas Santajuana <vnavas_7@hotmail.com>
2018-09-24 00:36:47 +0100

    Initial Commit

---
+++ VictorNavas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VictorNavas
+//
+//  Created by Victor Navas on 24/09/2018.
+//  Copyright © 2018 Victor Navas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ VictorNavasTests/VictorNavasTests.swift
@@ -0,0 +1,34 @@
+//
+//  VictorNavasTests.swift
+//  VictorNavasTests
+//
+//  Created by Victor Navas on 24/09/2018.
+//  Copyright © 2018 Victor Navas. All rights reserved.
+//
+
+import XCTest
+@testable import VictorNavas
+
+class VictorNavasTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
